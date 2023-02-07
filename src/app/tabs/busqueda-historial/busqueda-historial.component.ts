8435309eea3768c70330a3fe26b630563357f58f
Jose Javier <jsanudol@megacable.com.mx>
2023-02-06 21:02:43 -0700

    Creado historiales

--- src/app/tabs/busqueda-historial/busqueda-historial.component.ts
+++ src/app/tabs/busqueda-historial/busqueda-historial.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-busqueda-historial',
-  templateUrl: './busqueda-historial.component.html',
-  styleUrls: ['./busqueda-historial.component.scss'],
+    selector: 'app-busqueda-historial',
+    templateUrl: './busqueda-historial.component.html',
+    styleUrls: ['./busqueda-historial.component.scss'],
 })
 export class BusquedaHistorialComponent implements OnInit {
+    @Output() filtrar = new EventEmitter<{
+        desde: Date;
+        hasta: Date;
+    }>();
 
-  constructor() { }
+    public reactiveForm: FormGroup;
+    public hoy: Date = new Date();
 
-  ngOnInit() {}
+    constructor(private fb: FormBuilder) { }
 
+    ngOnInit() {
+        this.reactiveForm = this.fb.group({
+            desde: [null, Validators.required],
+            hasta: [null, Validators.required],
+        });
+    }
+
+    onFilter(){
+        if(this.reactiveForm?.valid){
+            this.filtrar.emit(this.reactiveForm.value);
+        }
+    }
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,22 +1,25 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
-import { DxDataGridModule } from 'devextreme-angular';
+import { DxDataGridModule, DxDateBoxModule } from 'devextreme-angular';
 import { TablaHistorialComponent } from './tabla-historial/tabla-historial.component';
+import { BusquedaHistorialComponent } from './busqueda-historial/busqueda-historial.component';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     TabsPageRoutingModule,
     DxDataGridModule,
+    DxDateBoxModule
   ],
-  declarations: [TabsPage, TablaHistorialComponent]
+  declarations: [TabsPage, TablaHistorialComponent, BusquedaHistorialComponent]
 })
 export class TabsPageModule {}
